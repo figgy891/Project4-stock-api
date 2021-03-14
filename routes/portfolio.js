947233bfa54d9c7d8f7184163953a715adfa4667
figgy891 <figgy891@gmail.com>
2021-03-14 14:17:12 -0500

    upload assignment

---
+++ routes/portfolio.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+const { Portfolio } = require('../lib/models');
+const yahooStockPrices = require('yahoo-stock-prices');
+//const mysql2 = require('mysql2');
+
+// DELETE - perform DELETE request on http://localhost:3000/api/v1/stocks/:id - STEP 1 Done | STEP 2 - Perform Actual Queries
+// UPDATE - perform PUT request on http://localhost:3000/api/v1/stocks/:id - STEP 1 Done | STEP 2 - Perform Actual Queries
+// CREATE - - perform POST request on http://localhost:3000/api/v1/stocks - STEP 1 Done | STEP 2 - Perform Actual Queries
+
+// NON-REST - CUSTOM
+// GET /api/v1/stocks/search?symbol=AAPL
+// GET /api/v1/stocks/search/AAPL
+// GET /api/v1/stocks/search/MSFT
+// POST /api/v1/stocks/search req.body
+
+router.get('/search/:symbol', async function (req, res, next) {
+    console.log(req.query)
+    console.log(req.params)
+    try {
+        const data = await yahooStockPrices.getCurrentData(req.params.symbol);
+        //const prices = await yahooStockPrices.getHistoricalPrices(0, 6, 2020, 0, 8, 2021, 'AAPL', '1d');
+        //res.json(prices);
+        res.json({ success: true, data: data });
+        // res.json(hist);
+    } catch (err) {
+        res.json({ success: false, data: {} });
+    }
+
+})
+
+// CREATE
+router.post('/', async function (req, res, next) {
+    
+    console.log(req.body)
+    let stock = await Portfolio.create(req.body);
+    // // Stock.create(req.body)
+    // //     .then((stock) => {
+    // //         res.json({success: true});
+    // //     })
+
+    res.json(stock);
+});
+
+// UPDATE
+router.put('/:id', async function (req, res, next) {
+    console.log(req.body)
+    console.log(req.params)
+    // let stock = await Stock.update()
+    // let stock = Stock.update(req.body, {where: req.params.id})
+    let stock = await Portfolio.update(req.body, {
+        where: { id: req.params.id }
+    });
+    res.json(stock);
+});
+
+// DELETE
+router.delete('/:id', async function (req, res, next) {
+    // console.log(req.params)
+    let dog = await Portfolio.destroy({ where: { id: req.params.id } });
+    res.json(dog);
+});
+
+
+/* GET users listing. */
+router.get('/', async function (req, res, next) {
+
+    console.log('I was here *******');
+
+    let items = await Portfolio.findAll({})
+
+    console.log(items);
+    res.send(items);
+});
+
+module.exports = router;
--- routes/wallet.js
+++ routes/wallet.js
@@ -6,12 +6,34 @@ const {Wallet} = require('../lib/models');
 
 // UPDATE - perform PUT request on http://localhost:3000/api/v1/wallet/:id
 
-
+router.put('/:id', async function(req, res, next) {
+    console.log(req.body)
+    console.log(req.params)
+    // let stock = await Stock.update()
+    // let stock = Stock.update(req.body, {where: req.params.id})
+    let wallet = await Wallet.update(req.body, {
+        where: {id: req.params.id}
+    });
+    res.json(wallet);
+});
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a stock');
+router.get('/', async function(req, res, next) {
+    let currentWallet = await Wallet.findOne({})
+    console.log(currentWallet)
+    res.json(currentWallet);
 });
 
 module.exports = router;
+
+router.put('/:id', async function(req, res, next) {
+    console.log(req.body)
+    console.log(req.params)
+    // let stock = await Stock.update()
+    // let stock = Stock.update(req.body, {where: req.params.id})
+    let wallet = await Wallet.update(req.body, {
+        where: {id: req.params.id}
+    });
+    res.json(wallet);
+});
